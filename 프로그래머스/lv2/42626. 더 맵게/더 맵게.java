f32fa5260560fddc36b158b2ef3f40aa0995f411
Jisoo Lee <93996283+1eejisoo@users.noreply.github.com>
2022-12-17 02:27:23 +0900

    [level 2] Title: 더 맵게, Time: 1838.57 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,34 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public static boolean isOverK(PriorityQueue<Integer> pq, int K) {
+
+        for (Integer i : pq) {
+            if (i < K) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int data, answer = 0;
+
+        for (int i : scoville) {
+            pq.add(i);
+        }
+
+        if (isOverK(pq, K)) return 0;
+
+        while (!isOverK(pq, K)){
+            if(pq.size() < 2) return -1;
+            
+            data = pq.poll() + pq.poll() * 2;
+            pq.add(data);
+            answer += 1;
+        }
+        
+        return answer;
+    }
+}
