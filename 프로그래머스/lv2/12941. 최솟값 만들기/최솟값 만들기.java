d84873b30f29db18732cc5dd7ad690f6e3512e17
Jisoo Lee <93996283+1eejisoo@users.noreply.github.com>
2022-12-16 17:02:11 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.62 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,22 +1,17 @@
 import java.util.Arrays;
-import java.util.Collections;
 
 class Solution
 {
     public int solution(int []A, int []B)
     {
-        Integer[] arr = new Integer[B.length];
         int answer = 0;
-        
-
-        for (int i = 0; i < B.length; i++)
-            arr[i] = B[i];
+        int index = B.length - 1;
 
         Arrays.sort(A);
-        Arrays.sort(arr, Collections.reverseOrder());
+        Arrays.sort(B);
 
-        for (int i = 0; i < A.length; i++) {
-            answer += A[i] * arr[i];
+        for (int data : A) {
+            answer += data * B[index--];
         }
 
         return answer;
