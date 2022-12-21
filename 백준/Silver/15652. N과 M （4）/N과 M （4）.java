2712165beb1caf81315e40cfe097bae798e96d0c
Jisoo Lee <93996283+1eejisoo@users.noreply.github.com>
2022-12-21 21:01:56 +0900

    [Silver III] Title: N과 M (4), Time: 172 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void combination(int[] out, int depth, int start, int n, int r) {
+        if (depth == r) {
+            for (int o : out) {
+                sb.append(o).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            out[depth] = i + 1;
+            combination(out, depth + 1, i, n, r);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] out = new int[M];
+
+        combination(out, 0, 0, N, M);
+
+        System.out.print(sb);
+    }
+}
