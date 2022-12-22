26f732ee751ed8781ee137b6afcaca1c6d7ccd38
Jisoo Lee <93996283+1eejisoo@users.noreply.github.com>
2022-12-22 19:20:45 +0900

    [Silver I] Title: 절댓값 힙, Time: 332 ms, Memory: 25400 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(((o1, o2) ->
+                Math.abs(o1) == Math.abs(o2) ? Integer.compare(o1, o2) : Integer.compare(Math.abs(o1),  Math.abs(o2)))
+        );
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int x;
+
+        for (int i = 0; i < N; i++) {
+            x = Integer.parseInt(br.readLine());
+
+            if (x != 0) {
+                pq.add(x);
+                continue;
+            }
+            if (pq.isEmpty()) {
+                sb.append(0).append("\n");
+                continue;
+            }
+            sb.append(pq.poll()).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
