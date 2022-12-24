fcc869f3cb2a3894a26ca959d886f23b2b65938c
Jisoo Lee <93996283+1eejisoo@users.noreply.github.com>
2022-12-25 00:40:27 +0900

    [Silver V] Title: 단어 정렬, Time: 368 ms, Memory: 26292 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -9,22 +9,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        String word;
-        List<String> words = new ArrayList<>();
+        HashSet<String> words = new HashSet<>();
 
         for (int i = 0; i < N; i++) {
-            word = br.readLine();
-            if (!words.contains(word))
-                words.add(word);
+            words.add(br.readLine());
         }
 
-        words.sort((o1, o2) -> {
+        words.stream().sorted((o1, o2) -> {
             if (o1.length() == o2.length())
                 return o1.compareTo(o2);
             return o1.length() - o2.length();
-        });
-
-        words.forEach(w -> sb.append(w).append("\n"));
+        }).forEach(s -> sb.append(s).append("\n"));
+        
         System.out.print(sb);
     }
 }
