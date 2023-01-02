8b019dc9921c5bf6bb44117476eee9e6de7017f8
Jisoo Lee <93996283+1eejisoo@users.noreply.github.com>
2023-01-03 03:32:05 +0900

    [Silver II] Title: 연속합, Time: 276 ms, Memory: 24096 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n + 1];
+        dp = new int[n + 1];
+        int max;
+
+        for (int i = 1; i <= n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        max = dp[1] = arr[1];
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+        System.out.print(max);
+    }
+}
