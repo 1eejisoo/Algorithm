e93fbd6e888124b85225d0cc28d2aba0524282d0
Jisoo Lee <93996283+1eejisoo@users.noreply.github.com>
2023-01-13 17:43:30 +0900

    [Gold III] Title: 줄 세우기, Time: 1020 ms, Memory: 53676 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static List<Integer>[] adj;
+    static int[] deg;
+    static Queue<Integer> q = new LinkedList<>();
+
+    static void topological_sort() {
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            System.out.print(cur + " ");
+            for (int nxt : adj[cur]) {
+                deg[nxt]--;
+                if (deg[nxt] == 0) q.offer(nxt);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        adj = new ArrayList[n + 1];
+        deg = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int in, out;
+            in = Integer.parseInt(st.nextToken());
+            out = Integer.parseInt(st.nextToken());
+            adj[in].add(out);
+            deg[out]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (deg[i] == 0) q.offer(i);
+        }
+
+        topological_sort();
+    }
+}
