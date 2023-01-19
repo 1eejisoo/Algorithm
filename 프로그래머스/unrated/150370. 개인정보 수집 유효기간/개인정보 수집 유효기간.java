4c44a7477a825c5915ca43fc7fc7a2a4c2c51548
Jisoo Lee <93996283+1eejisoo@users.noreply.github.com>
2023-01-20 00:49:02 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 1.57 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -6,12 +6,10 @@ class Solution {
 
     static List<HashMap<String, Integer>> termsList = new ArrayList<>();  // 약관정보 리스트
 
-    public boolean isExpired(String today, String privacy_day, String type) {
-        String[] today_arr = today.split("\\.");
+    public boolean isExpired(int today, String privacy_day, String type) {
+
         String[] day_arr = privacy_day.split("\\.");
-        int year = Integer.parseInt(day_arr[0]);
-        int month = Integer.parseInt(day_arr[1]);
-        int day = Integer.parseInt(day_arr[2]);
+        int day =  (Integer.parseInt(day_arr[0]) * 12 * 28) + (Integer.parseInt(day_arr[1]) * 28) + Integer.parseInt(day_arr[2]);
         int term = 0;  // 유효기간 달 수
 
         for (HashMap<String, Integer> hm : termsList) {
@@ -20,31 +18,16 @@ class Solution {
                 break;
             }
         }
+        day += term * 28;
 
-        year += term / 12;
-        month += term % 12;
-        day -= 1;
-
-        if (month > 12) {
-            year += 1;
-            month -= 12;
-        }
-        if (day == 0) {
-            day = 28;
-            month -= 1;
-        }
-
-        if (year < Integer.parseInt(today_arr[0])) return true;
-        if (year > Integer.parseInt(today_arr[0])) return false;
-        if (month < Integer.parseInt(today_arr[1])) return true;
-        if (month > Integer.parseInt(today_arr[1])) return false;
-        
-        return day < Integer.parseInt(today_arr[2]);
+        return today >= day;
     }
 
     public int[] solution(String today, String[] terms, String[] privacies) {
 
         List<Integer> answerList = new ArrayList<>();
+        String[] today_arr = today.split("\\.");
+        int t = Integer.parseInt(today_arr[0]) * 12 * 28 + Integer.parseInt(today_arr[1]) * 28 + Integer.parseInt(today_arr[2]);
 
         for (int i = 0; i < terms.length; i++) {
             String[] term = terms[i].split(" ");
@@ -54,7 +37,7 @@ class Solution {
 
         for (int i = 0; i < privacies.length; i++) {
             String[] privacy = privacies[i].split(" ");
-            if (isExpired(today, privacy[0], privacy[1]))
+            if (isExpired(t, privacy[0], privacy[1]))
                 answerList.add(i + 1);
         }
 
