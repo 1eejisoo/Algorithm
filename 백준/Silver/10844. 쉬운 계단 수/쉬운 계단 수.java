13e95c2110bd1e92cf73e68c54e4b373b49d07f3
Jisoo Lee <93996283+1eejisoo@users.noreply.github.com>
2023-01-22 04:06:43 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -16,9 +16,9 @@ public class Main {
         for (int i = 2; i <= n; i++) {
             for (int j = 0; j < 10; j++) {
                 if (j == 0) {
-                    dp[i][0] = dp[i - 1][1] % 1000000000;
+                    dp[i][j] = dp[i - 1][j + 1] % 1000000000;
                 } else if (j == 9) {
-                    dp[i][9] = dp[i - 1][8] % 1000000000;
+                    dp[i][j] = dp[i - 1][j - 1] % 1000000000;
                 } else {
                     dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
                 }
