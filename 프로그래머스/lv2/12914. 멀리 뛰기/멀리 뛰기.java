450e508c6bbfefdd7c4f7eede8f12d3735f947c4
Jisoo Lee <93996283+1eejisoo@users.noreply.github.com>
2023-01-21 20:22:07 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+    public long solution(int n) {
+        long[] dp = new long[2001];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        if (n < 3) return dp[n];
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+
+        return dp[n];
+    }
+}
