f8ce01589192b8e269cabe3546000c15ab05ed0a
Jisoo Lee <93996283+1eejisoo@users.noreply.github.com>
2023-01-24 15:40:00 +0900

    [Silver IV] Title: 주몽, Time: 212 ms, Memory: 16608 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int start = 0, end = n - 1;
+        while (start < end) {
+            int sum = arr[start] + arr[end];
+
+            if (sum < m) {
+                start++;
+            } else {
+                end--;
+            }
+            if (sum == m) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
